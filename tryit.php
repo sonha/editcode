e9ae7cca9037ed42528584bb919902bd135ba984
Ha Anh Son <hason61vn@gmail.com>
2014-05-07 17:59:16 +0700

    add jQuery demo

--- tryit.php
+++ tryit.php
@@ -1,22 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
-</script>
-<script>
-$(document).ready(function(){
-  $("#p1").hover(function(){
-    alert("You entered p1!");
-    },
-    function(){
-    alert("Bye! You now leave p1!");
-  }); 
-});
-</script>
-</head>
-<body>
-
-<p id="p1">This is a paragraph.</p>
-
-</body>
-</html>
+<?php 
+$link = mysql_connect('192.168.7.19:3307', 'mobi_sg19_x17', 'sklsBonC2W7Ba4R5bHBw');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+if (!mysql_select_db('db_mobi_sg19_x17', $link)) {
+    echo 'Could not select database';
+    exit;
+}
+$sql    = 'SELECT * FROM ct_giftcode_500 WHERE user_id <> ""';
+//$sql    = 'SELECT * FROM category WHERE id = 6';
+$result = mysql_query($sql, $link);
+if (!$result) {
+    echo "DB Error, could not query the database\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+?>
+<table border='1'>
+            <tr>
+    			<td>ID</td>
+				<td>Name</td>
+				<td>Status</td>
+			</tr>
+            <?php while ($row = mysql_fetch_assoc($result)) { ?>  
+            <tr>
+    			<td><?php echo $row['user_id'];?></td>
+				<td><?php echo $row['created_date'];?></td>
+				<td><?php echo $row['code'];?></td>
+			</tr>
+            <?php } ?>
+</table>
+<?php 
+mysql_free_result($result);
+mysql_close($link);
+?>
